06a84289271b466342a02a425cda204374bf3ffc
nasrimohtadi@gmail.com <Awkward@009>
2020-03-22 14:00:31 +0100

    Math Exception

--- src/main/java/com/springboot/springbootexception/controller/UserController.java
+++ src/main/java/com/springboot/springbootexception/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.springboot.springbootexception.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -13,5 +15,13 @@ public class UserController {
         java.lang.ArithmeticException: / by zero*/
         int num = 10 / 0;
         return "add";
+    }
+
+    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+    public ModelAndView handlerArithmeticException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("mathError");
+        return modelAndView;
     }
 }
