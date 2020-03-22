5a2cb0a0785521b276cf59d512b3c86c825282ce
nasrimohtadi@gmail.com <Awkward@009>
2020-03-22 14:48:44 +0100

    HandlerExceptionResolver configuration

--- src/main/java/com/springboot/springbootexception/exceptions/MyHandlerExceptionResolver.java
+++ src/main/java/com/springboot/springbootexception/exceptions/MyHandlerExceptionResolver.java
@@ -1,23 +1,27 @@
 package com.springboot.springbootexception.exceptions;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Configuration
 public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
 
     @Override
-    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        if (e instanceof ArithmeticException) {
-            modelAndView.setViewName("mathError");
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest,
+                                         HttpServletResponse httpServletResponse, @Nullable Object o, Exception e) {
+        ModelAndView mv = new ModelAndView();
+        if (e instanceof java.lang.ArithmeticException) {
+            mv.setViewName("mathError");
         }
-        if (e instanceof NullPointerException) {
-            modelAndView.setViewName("nullPointerError");
+        if (e instanceof java.lang.NullPointerException) {
+            mv.setViewName("nullPointerError");
         }
-        modelAndView.addObject("exception", e.toString());
-        return modelAndView;
+        mv.addObject("exception", e.toString());
+        return mv;
     }
 }
